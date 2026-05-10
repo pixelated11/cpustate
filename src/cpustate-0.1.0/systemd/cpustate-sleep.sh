6cbc8837d5a099d9f9b14acab9fbb177684af425
Andrew <andaru.haryo@gmail.com>
2026-05-10 09:57:27 +0700

    Delete src directory

--- src/cpustate-0.1.0/systemd/cpustate-sleep.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# Placed in /usr/lib/systemd/system-sleep/cpustate
-# Called by systemd-sleep on suspend/resume events
-# $1 = pre (going to sleep) or post (waking up)
-
-if [[ "$1" == "post" ]]; then
-    /usr/bin/cpustate daemon
-fi
